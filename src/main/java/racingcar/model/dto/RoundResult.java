1355fc9aacf6bfd255a32e344d0a7015763d41de
tlaqk229 <tlaqk229@gmail.com>
2022-10-10 02:29:31 +0900

    refactor(vo/dto): 데이터 객체 불필요한 생성자, setter 제거

--- src/main/java/racingcar/model/dto/RoundResult.java
+++ src/main/java/racingcar/model/dto/RoundResult.java
@@ -20,8 +20,4 @@ public class RoundResult {
     public List<CarState> getRoundResult() {
         return roundResult;
     }
-
-    public void setRoundResult(List<CarState> roundResult) {
-        this.roundResult = roundResult;
-    }
 }
--- src/main/java/racingcar/model/vo/CarState.java
+++ src/main/java/racingcar/model/vo/CarState.java
@@ -2,12 +2,12 @@ package racingcar.model.vo;
 
 public class CarState {
 
-    private String carName;
+    private final String carName;
     private int round;
     private int position;
 
-    public CarState(String carName) {
-        this.carName = carName;
+    public CarState(Car car) {
+        this.carName = car.getCarName();
     }
 
     public void addRound() {
@@ -18,31 +18,15 @@ public class CarState {
         this.position += add;
     }
 
-    public CarState(Car car) {
-        this.carName = car.getCarName();
-    }
-
     public String getCarName() {
         return carName;
-    }
-
-    public void setCarName(String carName) {
-        this.carName = carName;
     }
 
     public int getRound() {
         return round;
     }
 
-    public void setRound(int round) {
-        this.round = round;
-    }
-
     public int getPosition() {
         return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
     }
 }
