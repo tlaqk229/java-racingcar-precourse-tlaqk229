1d2857c16de64d6f5782369c1f3d5af47527f9fb
tlaqk229 <tlaqk229@gmail.com>
2022-10-10 00:50:49 +0900

    feat(domain): random값 추출, 전진 기능 추가

---
+++ src/main/java/racingcar/model/domain/Race.java
@@ -0,0 +1,22 @@
+package racingcar.model.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Race {
+
+    private static final int GO_FORWARD_BASE = 4;
+
+    private static final int BEGIN_NUMBER = 0; //임의생성 숫자 범위 최소값
+    private static final int END_NUMBER = 9; //임의생성 숫자 범위 최대값
+
+    public static int createRandomNumber() {
+        return Randoms.pickNumberInRange(BEGIN_NUMBER, END_NUMBER);
+    }
+
+    public static int goForward(int number) {
+        if (number >= GO_FORWARD_BASE) {
+            return 1;
+        }
+        return 0;
+    }
+}
